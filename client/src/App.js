df4d24e6122c231e6bbac1ed9cc662a6c5022273
richardhan <hxy.314@gmail.com>
2020-01-15 22:16:00 +1100

    feat(app): set up app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 
-function App() {
-	return <div className="App">react app</div>;
-}
+const cache = new InMemoryCache();
+const link = new HttpLink({
+	uri: 'http://localhost:4000/graphql',
+});
+
+const defaultOptions = {
+	watchQuery: {
+		fetchPolicy: 'cache',
+		errorPolicy: 'all',
+	},
+	query: {
+		fetchPolicy: 'cache',
+		errorPolicy: 'all',
+	},
+};
+
+const client = new ApolloClient({
+	cache,
+	link,
+	defaultOptions: defaultOptions,
+});
+
+const App = () => {
+	return (
+		<ApolloProvider client={client}>
+			<div>header</div>
+			<div>body</div>
+			<Container maxWidth="sm">
+				<BrowserRouter>
+					<Switch></Switch>
+				</BrowserRouter>
+			</Container>
+		</ApolloProvider>
+	);
+};
 
 export default App;

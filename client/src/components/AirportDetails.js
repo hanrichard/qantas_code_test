2dbd84355148323100421fd3793b5e158344b414
richardhan <hxy.314@gmail.com>
2020-01-16 21:36:09 +1100

    feat(airportdetails): add logic

--- client/src/components/AirportDetails.js
+++ client/src/components/AirportDetails.js
@@ -48,33 +48,43 @@ const AirportDetails = props => {
         <Wrapper>
             <Card className="airportDetail__card">
                 <CardContent>
-                    <Typography variant="h4" component="h1" gutterBottom>
-                        Airport Name: {airport.airportName}
-                    </Typography>
-                    <Typography variant="h5" component="h2" gutterBottom>
-                        Location
-                    </Typography>
-                    <ul>
-                        <li>
-                            <Typography variant="body1" gutterBottom>
-                                latitude: {airport.location.latitude}{' '}
+                    {airport ? (
+                        <div>
+                            <Typography variant="h4" component="h1" gutterBottom>
+                                Airport Name: {airport.airportName}
                             </Typography>
-                        </li>
-                        <li>
-                            <Typography variant="body1" gutterBottom>
-                                longitude: {airport.location.longitude}{' '}
+                            <Typography variant="h5" component="h2" gutterBottom>
+                                Location
                             </Typography>
-                        </li>
-                    </ul>
-                    <Typography variant="h5" component="h2" gutterBottom>
-                        TimeZone
-                    </Typography>
+                            <ul>
+                                <li>
+                                    <Typography variant="body1" gutterBottom>
+                                        latitude: {airport.location.latitude}{' '}
+                                    </Typography>
+                                </li>
+                                <li>
+                                    <Typography variant="body1" gutterBottom>
+                                        longitude: {airport.location.longitude}{' '}
+                                    </Typography>
+                                </li>
+                            </ul>
+                            <Typography variant="h5" component="h2" gutterBottom>
+                                TimeZone
+                            </Typography>
 
-                    <ul>
-                        <li>
-                            <Typography variant="body1">{airport.city.timeZoneName}</Typography>
-                        </li>
-                    </ul>
+                            <ul>
+                                <li>
+                                    <Typography variant="body1">{airport.city.timeZoneName}</Typography>
+                                </li>
+                            </ul>
+                        </div>
+                    ) : (
+                        <div>
+                            <Typography variant="h4" component="h1" gutterBottom>
+                                No result
+                            </Typography>
+                        </div>
+                    )}
                 </CardContent>
             </Card>
             <Link className="airportDetail__button" to="/">
